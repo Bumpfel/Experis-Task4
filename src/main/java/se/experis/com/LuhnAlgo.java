d5cf04b03d79a57b39e90aabd1555efa299e57f7
Bumpfel <34800245+Bumpfel@users.noreply.github.com>
2020-10-13 14:00:19 +0200

    fix(algo): input size error

--- src/main/java/se/experis/com/LuhnAlgo.java
+++ src/main/java/se/experis/com/LuhnAlgo.java
@@ -5,21 +5,20 @@ public final class LuhnAlgo {
   public boolean isValidCreditCard(long cardNr, int checkSum) {
     String cardNrStr = "" + cardNr;
 
-    if(cardNrStr.length() != 16) {
+    if(cardNrStr.length() != 15) {
       return false;
     }
 
     int sum = 0;
     for(int i = cardNrStr.length() - 1; i >= 0; i --) {
       int digit = Integer.parseInt(cardNrStr.substring(i, i + 1));
-      int calc = digit * (i % 2 == 1 ? 2 : 1);
+      int calc = digit * (i % 2 == 0 ? 2 : 1);
       if(calc > 9) {
         calc -= 9;
       }
       sum += calc;
     }
     int calculatedChecksum = sum * 9 % 10;
-   
     return calculatedChecksum == checkSum;
   }
 }
