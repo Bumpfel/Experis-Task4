62fa7a3a1dd259b488253104c934e04bb9aead19
Bumpfel <34800245+Bumpfel@users.noreply.github.com>
2020-10-13 16:49:09 +0200

    feat: split methods to make it easier to test

--- src/main/java/se/experis/com/LuhnAlgo.java
+++ src/main/java/se/experis/com/LuhnAlgo.java
@@ -2,23 +2,49 @@ package se.experis.com;
 
 public final class LuhnAlgo {
 
-  public boolean isValidCreditCard(long cardNr, int checkSum) throws RuntimeException {
-    String cardNrStr = "" + cardNr;
+  private final int CREDIT_CARD_LENGTH = 16;
 
-    if(cardNrStr.length() != 15) {
-      throw new RuntimeException("Erroneous input. Credit card number is not long enough");
-    }
+  public int calcCheckSum(long cardNr) {
+    String cardNrString = "" + cardNr;
+    int len = cardNrString.length();
 
     int sum = 0;
-    for(int i = cardNrStr.length() - 1; i >= 0; i --) {
-      int digit = Integer.parseInt(cardNrStr.substring(i, i + 1));
-      int calc = digit * (i % 2 == 0 ? 2 : 1);
+    for(int i = cardNrString.length() - 2; i >= 0; i --) {
+      int digit = Integer.parseInt(cardNrString.substring(i, i + 1));
+
+      boolean doubleValue = (len - i) % 2 == 0;
+      int calc = digit * (doubleValue ? 2 : 1);
+      
       if(calc > 9) {
         calc -= 9;
       }
       sum += calc;
     }
+    
     int calculatedChecksum = sum * 9 % 10;
-    return calculatedChecksum == checkSum;
+    System.out.println(calculatedChecksum);
+    
+    return calculatedChecksum;
+  }
+  
+  public boolean isValidCheckSum(long cardNr) {
+    long checkSum = cardNr % 10;   
+    return calcCheckSum(cardNr) == checkSum;
+  }
+
+  public boolean isValidCreditCardLength(long cardNr) {
+    return ("" + cardNr).length() == CREDIT_CARD_LENGTH;
+  }
+
+  public boolean isValidCreditCard(long cardNr) {
+    if(!isValidCreditCardLength(cardNr) || !isPositive(cardNr)) {
+      return false;
+    }
+
+    return isValidCheckSum(cardNr);
+  }
+
+  public boolean isPositive(long nr) {
+    return nr >= 0;
   }
 }
